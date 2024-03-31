c245c3929a1873728fba7dce25288d5843e84859
Justin Tan <justintws10@gmail.com>
2024-03-31 16:54:09 +1100

    completed chapter 10

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
+    end
   end
 end
